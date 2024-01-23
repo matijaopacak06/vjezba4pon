84fac5e5fddfee98e6d3336329822e10b237b633
matijaopacak06 <153612293+matijaopacak06@users.noreply.github.com>
2024-01-23 12:52:56 +0100

    Add files via upload

---
+++ vjezba4.ponavljanje.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace vj4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite cijenu bez PDV-a: ");
+            double cijenaBezPDV = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Unesite stopu PDV-a (%): ");
+            double stopaPDV = Convert.ToDouble(Console.ReadLine());
+
+            double iznosPDV = cijenaBezPDV * (stopaPDV / 100);
+            double cijenaSaPDV = cijenaBezPDV + iznosPDV;
+
+            Console.WriteLine("Iznos PDV-a: " + iznosPDV);
+            Console.WriteLine("Cijena sa PDV-om: " + cijenaSaPDV);
+            Console.WriteLine();
+        }
+    }
+}
+
